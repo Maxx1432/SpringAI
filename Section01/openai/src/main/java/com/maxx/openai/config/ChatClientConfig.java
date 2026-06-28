ff7e24418cb4bd81d76e40ea4c50539b49608143
Mahendra <mahendra.chawla26@gmail.com>
2026-06-28 18:30:57 +0530

    Mahendra | introducing the default and system message

---
+++ Section01/openai/src/main/java/com/maxx/openai/config/ChatClientConfig.java
@@ -0,0 +1,20 @@
+package com.maxx.openai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChatClientConfig {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
+        return chatClientBuilder.defaultSystem("""
+                        You are an internal HR helpdesk assistant.
+                        Your role is to assist employees with HR-related queries such as leave policies, attendance, payroll, benefits, reimbursement, onboarding, offboarding, employment verification, company policies, and other general HR support requests.
+                        If a user requests help with anything outside of these responsibilities, respond politely and inform them that you are only able to assist with HR-related tasks within your defined scope.
+                        """)
+                .defaultUser("How can you help me ?")
+                .build();
+    }
+}
