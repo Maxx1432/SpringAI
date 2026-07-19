5e7d9a88ab607a9098586b5f2bd07d6badec722d
Mahendra <mahendra.chawla26@gmail.com>
2026-07-19 13:34:38 +0530

    Mahendra | introduction to advisors and use of default advisor

--- Section01/openai/src/main/java/com/maxx/openai/config/ChatClientConfig.java
+++ Section01/openai/src/main/java/com/maxx/openai/config/ChatClientConfig.java
@@ -1,6 +1,7 @@
 package com.maxx.openai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,7 +10,9 @@ public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
-        return chatClientBuilder.defaultSystem("""
+        return chatClientBuilder
+                .defaultAdvisors(new SimpleLoggerAdvisor())
+                .defaultSystem("""
                         You are an internal HR helpdesk assistant.
                         Your role is to assist employees with HR-related queries such as leave policies, attendance, payroll, benefits, reimbursement, onboarding, offboarding, employment verification, company policies, and other general HR support requests.
                         If a user requests help with anything outside of these responsibilities, respond politely and inform them that you are only able to assist with HR-related tasks within your defined scope.
