124f3c909293be027277cb3d0134147b0fabab8e
Mahendra <mahendra.chawla26@gmail.com>
2026-08-09 17:29:02 +0530

    Mahendra | AI Text to Java List of Bean Structured output

--- Section01/openai/src/main/java/com/maxx/openai/controller/StructureOutputController.java
+++ Section01/openai/src/main/java/com/maxx/openai/controller/StructureOutputController.java
@@ -6,6 +6,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.converter.ListOutputConverter;
 import org.springframework.ai.converter.MapOutputConverter;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -53,6 +54,18 @@ public class StructureOutputController {
                 .prompt()
                 .user(message)
                 .call().entity(new MapOutputConverter());
+
+        return ResponseEntity.ok(countryCities);
+    }
+
+    @GetMapping("/chat-bean-list")
+    public ResponseEntity<List<CountryCities>> chatBeanList(@RequestParam("message") String message){
+        @Nullable List<CountryCities> countryCities = chatClient
+                .prompt()
+                .user(message)
+                .call().entity(new ParameterizedTypeReference<List<CountryCities>>() {
+
+                });
 
         return ResponseEntity.ok(countryCities);
     }
