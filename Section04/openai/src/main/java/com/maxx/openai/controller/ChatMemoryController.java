f95cf7826bdf013f3e0bd24f594d7221d351b5d8
Mahendra <mahendra.chawla26@gmail.com>
2026-08-30 13:34:38 +0530

    Mahendra | Per user Memory in LLMs, by providing CONVERSATION_ID

--- Section04/openai/src/main/java/com/maxx/openai/controller/ChatMemoryController.java
+++ Section04/openai/src/main/java/com/maxx/openai/controller/ChatMemoryController.java
@@ -4,6 +4,7 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,8 +20,8 @@ public class ChatMemoryController {
     }
 
     @RequestMapping("/chat-memory")
-    public ResponseEntity<String> chatMemory(@RequestParam("message")  String message){
+    public ResponseEntity<String> chatMemory(@RequestHeader("username") String username, @RequestParam("message")  String message){
         return ResponseEntity.ok(chatClient.prompt().user(message)
-                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, "default")).call().content());
+                .advisors(advisorSpec -> advisorSpec.param(ChatMemory.CONVERSATION_ID, username)).call().content());
     }
 }
